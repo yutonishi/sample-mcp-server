80f745ee7249419d758c6a91385231722fc4b90d
yutonishi <yuto.nishi10@gmail.com>
2025-04-06 16:29:19 +0900

    🎉 first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+// Create an MCP server
+const server = new McpServer({
+  name: "sample-mcp-server",
+  version: "1.0.0"
+});
+
+// Simple tool with parameters
+server.tool(
+  "calculate-bmi",
+  {
+    weightKg: z.number(),
+    heightM: z.number()
+  },
+  async ({ weightKg, heightM }) => ({
+    content: [{
+      type: "text",
+      text: String(weightKg / (heightM * heightM))
+    }]
+  })
+);
+
+// Running with stdio ( for Command )
+const transport = new StdioServerTransport();
+await server.connect(transport);
